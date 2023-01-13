f03bc5b04b8efc409fee72b23ce8dd6c2af84d13
Jeremy Busk <jeremybusk@gmail.com>
2023-01-13 11:10:48 -0700

    fix nginx add scripts

--- main.sh
+++ main.sh
@@ -2,10 +2,11 @@
 set -eu
 
 docker_build_push(){
-  docker build --tag $org/$name:latest --tag $org/$name:$release --tag "$org/$name:$release-$os" .
+  # nocache="--no-cache"
+  nocache=""
+  docker build $nocache --tag $org/$name:latest --tag $org/$name:$release --tag "$org/$name:$release-$os" .
   echo Push to docker repo in 5 seconds; sleep 5
   echo $DOCKERHUB_USERTOKEN | docker login -u $DOCKERHUB_USERNAME --password-stdin
-  # echo $DOCKERHUB_TOKEN | docker login --username $DOCKERHUB_USERNAME --password-stdin
   docker push $org/$name:$release
   docker push $org/$name:latest
   docker push "$org/$name:$release-$os"
@@ -13,13 +14,18 @@ docker_build_push(){
 }
 
 containers=(
-"nginx"
+  "nginx"
 )
 
-for container in "${containers[@]}"; do
-  echo $container
-  cd $container
-  . .env
-  docker_build_push
-  cd ../
-done
+main(){
+  for container in "${containers[@]}"; do
+    echo $container
+    cd $container
+    . .env
+    ../scripts/process-templates.sh
+    docker_build_push
+    cd ../
+  done
+}
+
+main
