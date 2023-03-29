f418d768d55c715fe981c7391ee19d12e99886b8
Jeremy Busk <jeremybusk@gmail.com>
2023-03-29 08:38:59 -0600

    cleanup

--- uvoo/email-relay/test-email.sh
+++
@@ -1,3 +0,0 @@
-./scripts/client.py -H localhost -P 8587 -f no-reply@uvoo.me -t jeremybusk@gmail.com -u tester@localhost -p PleaseChangeMe
-# ./scripts/client.py -H localhost -P 8587 -f no-reply@uvoo.me -t jeremybusk@gmail.com -n -N
-# ./scripts/client.py -H localhost -P 8587 -f no-reply@uvoo.me -t jeremybusk@gmail.com -N
