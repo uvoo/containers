fcecd9b7b0ad9f5878e8c56a89b78ecbf958946a
Jeremy Busk <jeremybusk@gmail.com>
2023-10-18 21:08:42 -0600

    update naming

--- uvoo/certstrap-ssh/main.sh
+++ uvoo/certstrap-ssh/main.sh
@@ -2,15 +2,24 @@
 set -eu
 echo "Starting certstrap-ssh."
 
-file="${APP_USERDIR}/.ssh/authorized_keys"
+file="${ADMIN_DIR}/.ssh/authorized_keys"
 if [[ -v AUTHORIZED_KEYS ]]; then
-    sudo -u ${APP_USERNAME} mkdir -p ${APP_USERDIR}/.ssh
-    echo "${AUTHORIZED_KEYS}" | sudo -u ${APP_USERNAME} tee $file
+    sudo -u ${ADMIN_USERNAME} mkdir -p ${ADMIN_DIR}/.ssh
+    echo "${AUTHORIZED_KEYS}" | sudo -u ${ADMIN_USERNAME} tee $file
 fi
 
-if [[ -v USERPASSWORD ]]; then
-  echo "Setting ${APP_USERNAME} password."
-  set +x && echo "${APP_USERNAME}:${APP_USERPASSWORD}" | chpasswd
+if [[ -v ADMIN_PASSWORD ]]; then
+  echo "Setting ${ADMIN_USERNAME} password."
+  set +x && echo "${ADMIN_USERNAME}:${ADMIN_PASSWORD}" | chpasswd
+fi
+
+if [[ -v SSH_HOST_ECDSA_KEY ]]; then
+  echo "$SSH_HOST_ECDSA_KEY" > /etc/sshd_config/ssh_host_ecdsa_key
+  echo "$SSH_HOST_ECDSA_KEY_PUB" > /etc/sshd_config/ssh_host_ecdsa_key.pub
+  echo "$SSH_HOST_RSA_KEY" > /etc/sshd_config/ssh_host_rsa_key
+  echo "$SSH_HOST_RSA_KEY_PUB" > /etc/sshd_config/ssh_host_rsa_key.pub
+  echo "$SSH_HOST_ED25519_KEY" > /etc/sshd_config/ssh_host_ed25519_key
+  echo "$SSH_HOST_ED25519_KEY_PUB" > /etc/sshd_config/ssh_host_ed25519_key.pub
 fi
 
 /usr/sbin/sshd -D -e
