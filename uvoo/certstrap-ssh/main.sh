432299ca316efff6fd2839e116d7249f4b2541c1
Jeremy Busk <jeremybusk@gmail.com>
2023-10-18 21:29:44 -0600

    add host keys

--- uvoo/certstrap-ssh/main.sh
+++ uvoo/certstrap-ssh/main.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -eu
+set -eux
 echo "Starting certstrap-ssh."
 
 file="${ADMIN_DIR}/.ssh/authorized_keys"
@@ -14,12 +14,16 @@ if [[ -v ADMIN_PASSWORD ]]; then
 fi
 
 if [[ -v SSH_HOST_ECDSA_KEY ]]; then
-  echo "$SSH_HOST_ECDSA_KEY" > /etc/sshd_config/ssh_host_ecdsa_key
-  echo "$SSH_HOST_ECDSA_KEY_PUB" > /etc/sshd_config/ssh_host_ecdsa_key.pub
-  echo "$SSH_HOST_RSA_KEY" > /etc/sshd_config/ssh_host_rsa_key
-  echo "$SSH_HOST_RSA_KEY_PUB" > /etc/sshd_config/ssh_host_rsa_key.pub
-  echo "$SSH_HOST_ED25519_KEY" > /etc/sshd_config/ssh_host_ed25519_key
-  echo "$SSH_HOST_ED25519_KEY_PUB" > /etc/sshd_config/ssh_host_ed25519_key.pub
+  echo "${SSH_HOST_ECDSA_KEY_PUB}" > /etc/sshd_config/ssh_host_ecdsa_key.pub
+  set +x
+  echo "${SSH_HOST_ECDSA_KEY}" > /etc/sshd_config/ssh_host_ecdsa_key
+  chmod 600 /etc/sshd_config/ssh_host_ecdsa_key
+  echo "${SSH_HOST_RSA_KEY}" > /etc/sshd_config/ssh_host_rsa_key
+  chmod 600 /etc/sshd_config/ssh_host_rsa_key
+  echo "${SSH_HOST_RSA_KEY_PUB}" > /etc/sshd_config/ssh_host_rsa_key.pub
+  echo "${SSH_HOST_ED25519_KEY}" > /etc/sshd_config/ssh_host_ed25519_key
+  chmod 600 /etc/sshd_config/ssh_host_ed25519_key
+  echo "${SSH_HOST_ED25519_KEY_PUB}" > /etc/sshd_config/ssh_host_ed25519_key.pub
 fi
 
 /usr/sbin/sshd -D -e
