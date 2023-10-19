ce60ca00a4b4b2d2732799737c3a0dacafa0a9d3
Jeremy Busk <jeremybusk@gmail.com>
2023-10-18 21:43:27 -0600

    remove verbose

--- uvoo/certstrap-ssh/main.sh
+++ uvoo/certstrap-ssh/main.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -eux
+set -eu
 echo "Starting certstrap-ssh."
 
 file="${ADMIN_DIR}/.ssh/authorized_keys"
@@ -15,16 +15,16 @@ fi
 
 if [[ -v SSH_HOST_ECDSA_KEY ]]; then
   mkdir -p /etc/sshd_config
-  echo "${SSH_HOST_ECDSA_KEY_PUB}" > /etc/sshd_config/ssh_host_ecdsa_key.pub
   set +x
+  echo "${SSH_HOST_ECDSA_KEY_PUB}" > /etc/sshd_config/ssh_host_ecdsa_key.pub
   echo "${SSH_HOST_ECDSA_KEY}" > /etc/sshd_config/ssh_host_ecdsa_key
   chmod 600 /etc/sshd_config/ssh_host_ecdsa_key
+  echo "${SSH_HOST_RSA_KEY_PUB}" > /etc/sshd_config/ssh_host_rsa_key.pub
   echo "${SSH_HOST_RSA_KEY}" > /etc/sshd_config/ssh_host_rsa_key
   chmod 600 /etc/sshd_config/ssh_host_rsa_key
-  echo "${SSH_HOST_RSA_KEY_PUB}" > /etc/sshd_config/ssh_host_rsa_key.pub
+  echo "${SSH_HOST_ED25519_KEY_PUB}" > /etc/sshd_config/ssh_host_ed25519_key.pub
   echo "${SSH_HOST_ED25519_KEY}" > /etc/sshd_config/ssh_host_ed25519_key
   chmod 600 /etc/sshd_config/ssh_host_ed25519_key
-  echo "${SSH_HOST_ED25519_KEY_PUB}" > /etc/sshd_config/ssh_host_ed25519_key.pub
 fi
 
 /usr/sbin/sshd -D -e
