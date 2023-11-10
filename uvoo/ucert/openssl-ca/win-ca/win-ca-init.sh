3c28e3bdd14fad45fffb4141caad99085fdcf7a8
Jeremy Busk <jeremybusk@gmail.com>
2023-11-10 08:55:32 -0700

    add win-ca-init

---
+++ uvoo/ucert/openssl-ca/win-ca/win-ca-init.sh
@@ -0,0 +1,133 @@
+#!/bin/bash
+set -aeux
+. dev.env
+
+cd ${PWD}
+openssl rand -writerand .rnd
+OPENSSL_CA_DIR=openssl_ca
+mkdir -p ${OPENSSL_CA_DIR}
+mkdir -p ${OPENSSL_CA_DIR}/certs
+mkdir -p ${OPENSSL_CA_DIR}/crl
+mkdir -p ${OPENSSL_CA_DIR}/private
+if [ ! -e ${OPENSSL_CA_DIR}/serial ]; then
+  echo 0100 > ${OPENSSL_CA_DIR}/serial
+fi
+if [ ! -e ${OPENSSL_CA_DIR}/index ]; then
+  touch ${OPENSSL_CA_DIR}/index
+fi
+
+cat << EOF > ./openssl_ca/openssl.cnf
+HOME                      = .
+RANDFILE                  = \$ENV::HOME/.rnd
+
+[ ca ]
+default_ca                = CA_default
+
+[ CA_default ]
+dir                       = ${PWD}/openssl_ca
+# dir                       = /mnt/c/Users/jebusk/src/sandbox/containers/uvoo/ucert/openssl-ca/openssl_ca
+crl_dir                   = \$dir/crl
+database                  = \$dir/index
+new_certs_dir             = \$dir/certs
+serial                    = \$dir/serial
+# certificate               = \$dir/issuer.crt
+certificate               = \$dir/RootCA1.crt
+private_key               = \$dir/RootCA1.key
+# certificate               = \$dir/ICA1a.crt
+# private_key               = \$dir/ICA1a.key
+# private_key               = \$dir/private/issuer.key
+# private_key               = \$dir/private/ICA1a.key
+policy                    = policy_match
+default_days              = 365           # 1 year
+default_crl_days          = 7             # 7 days
+default_md                = sha1
+default_bits              = 2048
+preserve                  = no
+unique_subject            = no
+x509_extensions           = v3_req
+copy_extensions           = copy          # to enable SubjectAltName
+
+[ policy_match ]
+countryName               = optional
+stateOrProvinceName       = optional
+localityName              = optional
+# organizationName          = supplied
+organizationName          = optional
+organizationalUnitName    = optional
+commonName                = optional
+
+[ req ]
+distinguished_name        = req_distinguished_name
+
+[ req_distinguished_name ]
+countryName               = Country (2 letter code)
+countryName_min           = 2
+countryName_max           = 2
+stateOrProvinceName       = State or Province (spelled out)
+localityName              = City or Locality
+organizationName          = Organization
+organizationalUnitName    = Organizational Unit
+commonName                = Common Name (FQDN)
+commonName_max            = 64
+
+[ v3_req ]
+basicConstraints          = CA:FALSE
+subjectKeyIdentifier      = hash
+authorityKeyIdentifier    = keyid,issuer
+keyUsage                  = digitalSignature,keyEncipherment
+extendedKeyUsage          = serverAuth,clientAuth
+crlDistributionPoints     = URI:http://pki.venafi.example/issuer.crl
+
+[ v3_ca ]
+basicConstraints          = CA:TRUE,pathlen:3
+subjectKeyIdentifier      = hash
+authorityKeyIdentifier    = keyid:always,issuer:always
+keyUsage                  = cRLSign,keyCertSign
+
+[ v3_intermediate_ca ]
+basicConstraints          = CA:TRUE,pathlen:3
+subjectKeyIdentifier      = hash
+authorityKeyIdentifier    = keyid:always,issuer:always
+keyUsage                  = cRLSign,keyCertSign
+EOF
+
+cd ${OPENSSL_CA_DIR}
+
+# RootCA1
+openssl genrsa -passout env:ROOTCA1_PWD -aes256 -out RootCA1.key 4096
+# openssl req -passin env:ROOTCA1_PWD -config openssl.cnf \
+#             -extensions v3_ca \
+openssl req -passin env:ROOTCA1_PWD \
+            -key RootCA1.key \
+            -new -x509 -days 3653 -sha256 -extensions v3_ca \
+            -out RootCA1.crt -subj "${ROOTCA_SUBJ}"
+openssl x509 -noout -text -in RootCA1.crt
+
+# IntermediateCA ICA1a
+openssl genrsa -passout env:ICA1a_PWD -aes256 -out ICA1a.key 4096
+# openssl req -passin env:ICA1a_PWD -config openssl.cnf \
+            # -addext basicConstraints=critical,CA:TRUE,pathlen:0 \
+# openssl req -passin env:ICA1a_PWD \
+openssl req -passin env:ICA1a_PWD -config openssl.cnf \
+            -new -sha256 \
+            -key ICA1a.key \
+            -out ICA1a.csr -subj "${ICA_SUBJ}"
+# yes | openssl ca -passin env:ROOTCA1_PWD -config openssl.cnf \
+           # -extensions v3_intermediate_ca \
+# yes | openssl ca -passin env:ROOTCA1_PWD \
+yes | openssl ca -passin env:ROOTCA1_PWD -config openssl.cnf \
+           -extensions v3_intermediate_ca \
+           -days 1826 -notext -md sha256 \
+           -in ICA1a.csr \
+           -out ICA1a.crt
+openssl x509 -text -in ICA1a.crt
+openssl x509 -text -in certs/0100.pem
+
+# openssl pkcs12 -export -passout env:ICA1A_PASSWORD -out ${CN_PREFIX}ICA1a.chain.pfx -passin env:ICA1A_PASSWORD -inkey ${CN_PREFIX}ICA1a.key -in ${CN_PREFIX}ICA1a.crt -certfile ${CN_PREFIX}ICA1a.chain
+
+cat RootCA1.crt > ICA1a.chain
+cat ICA1a.crt >> ICA1a.chain
+openssl pkcs12 -export -passout env:ICA1a_PWD -out ICA1a.pfx -passin env:ICA1a_PWD -inkey ICA1a.key -in ICA1a.crt -certfile ICA1a.chain
+
+echo "Completed successfully!"
+cd ../
