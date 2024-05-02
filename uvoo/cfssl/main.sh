1394b5855b7e5a322fd3890d002c5d4d65c5f41a
Jeremy Busk <jeremybusk@gmail.com>
2024-05-02 00:39:58 -0600

    fix cert ref in main

--- uvoo/cfssl/main.sh
+++ uvoo/cfssl/main.sh
@@ -3,4 +3,5 @@ set -eu
 echo "Starting ucfssl server ..."
 
 ./init-cfssl-ca.sh
-cfssl serve -db-config=db-config.json -ca=ca/ica1.crt -ca-key=ca/ica1.key.crt -config=config.json  -address=0.0.0.0 -port=3000
+openssl rsa -in ca/ica1.key.enc -out ica1.key -aes256 -passin pass:$INTERMEDIATE_CA_PASS
+cfssl serve -db-config=db-config.json -ca=ca/ica1.crt -ca-key=ica1.key -config=config.json  -address=0.0.0.0 -port=3000
