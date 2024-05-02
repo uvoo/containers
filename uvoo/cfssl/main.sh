5ab668a6568561f197c6151faba2cda0f1e88dc1
Jeremy Busk <jeremybusk@gmail.com>
2024-05-02 01:23:49 -0600

    remove aes256

--- uvoo/cfssl/main.sh
+++ uvoo/cfssl/main.sh
@@ -3,5 +3,5 @@ set -eu
 echo "Starting ucfssl server ..."
 
 ./init-cfssl-ca.sh
-openssl rsa -in ca/ica1.key.enc -out ica1.key -aes256 -passin pass:$INTERMEDIATE_CA_PASS
+openssl rsa -in ca/ica1.key.enc -out ica1.key -passin pass:$INTERMEDIATE_CA_PASS
 cfssl serve -db-config=db-config.json -ca=ca/ica1.crt -ca-key=ica1.key -config=config.json  -address=0.0.0.0 -port=3000
