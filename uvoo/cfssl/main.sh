b5163246bcaaf8d21098cacab241206f8f671a2b
Jeremy Busk <jeremybusk@gmail.com>
2024-05-03 22:58:00 -0600

    fix dir

--- uvoo/cfssl/main.sh
+++ uvoo/cfssl/main.sh
@@ -4,4 +4,4 @@ echo "Starting ucfssl server ..."
 
 ./init-cfssl-ca.sh
 openssl rsa -in ca/ica1.key.enc -out ica1.key -passin pass:$INTERMEDIATE_CA_PASS
-cfssl serve -db-config=db-config.json -ca=ca/ica1.crt -ca-key=ica1.key -config=config.json  -address=0.0.0.0 -port=3000
+cfssl serve -db-config=ca/db-config.json -ca=ca/ica1.crt -ca-key=ica1.key -config=config.json  -address=0.0.0.0 -port=3000
