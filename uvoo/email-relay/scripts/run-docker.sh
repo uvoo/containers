1276c1b3437c919a0fd377e83e37d794ccf20542
Jeremy Busk <jeremybusk@gmail.com>
2024-06-12 18:31:02 -0600

    use port 8587, rm envtpl

--- uvoo/email-relay/scripts/run-docker.sh
+++ uvoo/email-relay/scripts/run-docker.sh
@@ -4,20 +4,22 @@ cd data
 mkdir -p keys logs spool_postfix
 sudo chmod 0755 keys logs spool_postfix
 cd ../
-docker run -p 8587:25 \
+docker run -p 8587:8587 \
   -e SMTP_USERNAME=tester@localhost \
   -e SMTP_USERPASS=PleaseChangeMe \
   -e SMTP_USERS="tester@localhost:PleaseChangeMe tester1@localhost:PleaseChangeMe" \
   -e MYNETWORKS="127.0.0.0/8 172.16.0.0/12 10.210.77.1/32" \
-  -e DKIM_DOMAINS="uvoo.me" \
+  -e DKIM_DOMAINS="example.com" \
   -e DKIM_SELECTORS="default mail" \
-  -e MYHOSTNAME="mail.uvoo.me" \
+  -e MYHOSTNAME="mail.example.com" \
+  -e SMTP_TLS_SECURITY_LEVEL="may" \
   -v ${PWD}/data/keys:/etc/opendkim/keys \
   -v ${PWD}/data/logs:/var/log/mail\
   -v ${PWD}/data/spool_postfix:/var/spool/postfix \
-  --hostname mail.uvoo.me \
+  --hostname mail.example.com \
   --name postfix -d postfix
 
+# SMTP_TLS_SECURITY_LEVEL="may" none/may/encrypted default is encrypted
 # Other env vars
 # RELAY_HOST
 # RELAY_USERNAME
