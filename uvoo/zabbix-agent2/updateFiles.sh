a4438f6642096968b14277d511fa21ec20af8ef2
Jeremy Busk <jeremybusk@gmail.com>
2024-10-08 13:27:01 -0600

    bump zbx agent2

--- uvoo/zabbix-agent2/updateFiles.sh
+++ uvoo/zabbix-agent2/updateFiles.sh
@@ -3,6 +3,6 @@ set -e
 # git clone -b 6.0 https://github.com/zabbix/zabbix
 # cp -rp zabbix/templates/db/postgresql ./
 rm -rf postgresql
-git clone --depth 1 --branch 6.0.18 https://github.com/zabbix/zabbix
+git clone --depth 1 --branch 6.0.34 https://github.com/zabbix/zabbix
 cp -rp zabbix/templates/db/postgresql ./
 rm -rf zabbix
